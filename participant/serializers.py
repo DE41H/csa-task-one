d4da13d1da30af2d83ce7ea278c937012d8695f2
DE41H <sreyash.s@outlook.com>
2026-09-22 18:44:48 +0530

    fixed n+1 query

---
+++ participant/serializers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.db.transaction import atomic
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from .models import Hostel, Participant
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=150, validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = Participant
+        fields = "__all__"
+
+    @atomic
+    def create(self, validated_data):
+        user = User.objects.create_user(username=validated_data.pop("username"), password=validated_data.pop("password"))
+        return Participant.objects.create(user=user, **validated_data)
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    hostel = serializers.StringRelatedField()
+
+    class Meta:
+        model = Participant
+        fields = ["id", "handle", "hostel", "created_at", "updated_at"]
+        read_only_fields = fields
+
+
+class HostelSerializer(serializers.ModelSerializer):
+    score = serializers.IntegerField(read_only=True)
+    cracked_missions = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Hostel
+        fields = ["id", "name", "score", "cracked_missions", "created_at", "updated_at"]
+        read_only_fields = fields
+
+    def get_cracked_missions(self, obj):
+        return obj.cracked_missions.order_by("-points").values("codename", "points", "difficulty", "deadline")
--- participant/views.py
+++ participant/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
-# Create your views here.
+from mission.permissions import IsParticipant, IsReadOnly, IsStaff
+
+from .models import Hostel, Participant
+from .serializers import HostelSerializer, ParticipantSerializer, RegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class HostelViewSet(ModelViewSet):
+    queryset = Hostel.objects.annotate(score=Coalesce(Sum("cracked_missions__points"), 0)).order_by("-score", "name")
+    serializer_class = HostelSerializer
+    permission_classes = [IsReadOnly | IsStaff]
+
+
+class ParticipantViewSet(ReadOnlyModelViewSet):
+    queryset = Participant.objects.select_related("hostel", "user")
+    serializer_class = ParticipantSerializer
+    permission_classes = [IsParticipant | IsStaff]
