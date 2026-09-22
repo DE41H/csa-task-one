779dc19bfaceb5ded527d8715a95b882db89173a
DE41H <sreyash.s@outlook.com>
2026-09-22 16:10:49 +0530

    added auth

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from participant.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/api/register/', RegisterView.as_view(), name='register'),
     path('api/', include('mission.urls')),
     path('api/', include('participant.urls')),
 ]
