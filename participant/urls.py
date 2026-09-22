6b2f2fb24c4c5297ed96e8bbf3db2b3699c4e775
DE41H <sreyash.s@outlook.com>
2026-09-22 19:19:55 +0530

    added leaderboard url

--- participant/urls.py
+++ participant/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import HostelViewSet, ParticipantViewSet
@@ -6,4 +7,6 @@ router = DefaultRouter()
 router.register("hostels", HostelViewSet, basename="hostel")
 router.register("participants", ParticipantViewSet, basename="participant")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("leaderboard/", HostelViewSet.as_view({"get": "list"}), name="leaderboard"),
+]
