039b90c140a502b5a04b45d8811d89f141be30ee
DE41H <sreyash.s@outlook.com>
2026-09-22 16:51:46 +0530

    added IsParticipant permission

--- mission/permissions.py
+++ mission/permissions.py
@@ -9,3 +9,8 @@ class IsReadOnly(BasePermission):
 class IsStaff(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user.is_staff)
+
+
+class IsParticipant(BasePermission):
+    def has_permission(self, request, view):
+        return hasattr(request.user, "participant")
