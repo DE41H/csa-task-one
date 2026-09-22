8d5473ae719a63788a3baf1227ffa76be606ff53
DE41H <sreyash.s@outlook.com>
2026-09-22 16:47:05 +0530

    removed leaderboard view

--- participant/urls.py
+++ participant/urls.py
@@ -1,13 +1,9 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import HostelViewSet, LeaderboardView, ParticipantViewSet
+from .views import HostelViewSet, ParticipantViewSet
 
 router = DefaultRouter()
 router.register("hostels", HostelViewSet, basename="hostel")
 router.register("participants", ParticipantViewSet, basename="participant")
 
-urlpatterns = [
-    path("leaderboard/", LeaderboardView.as_view(), name="leaderboard"),
-    *router.urls,
-]
+urlpatterns = router.urls
