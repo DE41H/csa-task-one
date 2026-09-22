3fe4bd2b6eb2bcaaec7010da2d2803160eda68ef
DE41H <sreyash.s@outlook.com>
2026-09-22 21:50:32 +0530

    added cors

--- config/settings.py
+++ config/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'participant',
     'drf_spectacular',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,6 +159,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Operation Golden Keyboard API',
